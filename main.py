c390319b0d11c9cd49b84f0c689cf15db79f9447
RobinHeath-Albuquerque <79651450+RobinHeath-Albuquerque@users.noreply.github.com>
2021-03-25 10:57:16 -0600

    Cleaned up all syntax errors, added imports from each class

---
+++ main.py
@@ -0,0 +1,5 @@
+# This is a sample Python script.
+from simulation import Simulation, run_simulation
+
+simulation = Simulation()
+run_simulation()
---
+++ simulation.py
@@ -0,0 +1,25 @@
+import user_interface
+from customer import Customer, check_backpack
+from soda_machine import SodaMachine
+
+
+def run_simulation():
+    """The central method called in main.py."""
+    customer = Customer()
+    soda_machine = SodaMachine()
+    will_proceed = False
+    while will_proceed:
+        user_option = user_interface.simulation_main_menu()
+        if user_option == 1:
+            soda_machine.begin_transaction(customer)
+        elif user_option == 2:
+            customer.check_coins_in_wallet()
+        elif user_option == 3:
+            check_backpack()
+        else:
+            will_proceed = False
+
+
+class Simulation:
+    def __init__(self):
+        pass
